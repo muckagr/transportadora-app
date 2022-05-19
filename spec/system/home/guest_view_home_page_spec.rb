4da4f3dc8fd8e9bf28b5880a1b1e4750b211f5b0
muckagr <arthurafk@gmail.com>
2022-05-19 00:38:54 -0300

    visitante visita homepage

---
+++ spec/system/home/guest_view_home_page_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'Guest visits home page' do
+    it 'and see the product search bar' do
+
+        visit(root_path)
+        
+        expect(page).to have_field('Código de Rastreio:')
+        expect(page).to have_content('Rastreio de Produtos')
+        expect(page).to have_button('Procurar')
+    end
+end
